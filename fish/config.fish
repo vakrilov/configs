2ac47603aa0dcf4b118444219ad6faeff21dd518
vakrilov <alexander.vakrilov@telerik.com>
2020-03-09 16:02:40 +0200

    add fish configs

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+echo Loading config.fish...
+
+alias code="open -a Visual\ Studio\ Code"
+alias c="code ."
+alias g="git"
+alias ll="ls -al"
+alias ...="cd ../.."
+alias gh="open (git config remote.origin.url)"
